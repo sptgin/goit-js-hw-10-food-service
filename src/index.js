6bc74412ef405bb0399c4aeb2e46afb9777df156
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-08-11 03:00:46 +0300

    Добавляет index.js
    Добавляет index.js

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 console.log('Start index.js ....');
 
-import './sass/main.scss';
-import menuCardData from './menu.json';
-import menuCardTemplate from './templates/menucard.hbs';
-import { menuData } from './menu.json';
-const menuCardDataObject = JSON.parse(menuCardData);
+//import './sass/main.scss';
+//import menuCardData from './menu.json';
+//import menuCardTemplate from './templates/menucard.hbs';
+// import { menuData } from './menu.json';
+//const menuCardDataObject = JSON.parse('./menu.json');
 
-console.log(menuCardDataObject);
+//console.log(menuCardDataObject);
