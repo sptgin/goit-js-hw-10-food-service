232428d7b7e32e256ceb74e0f8da033b0a964401
Sergej Pjatygin <sergej.pjatygin@gmail.com>
2021-08-12 10:14:04 +0300

    Добавляет смену темы
    Добавляет смену темы

--- src/index.js
+++ src/index.js
@@ -4,3 +4,24 @@ import menuCardTemplate from './templates/menucard.hbs';
 const menuCard = document.querySelector('.js-menu');
 const menuCardInfo = menuCardData.map(menuCardTemplate).join(' ');
 menuCard.insertAdjacentHTML('beforeend', menuCardInfo);
+
+const themeSwitch = document.querySelector('.theme-switch__toggle');
+const menuTheme = document.querySelector('body');
+themeSwitch.addEventListener('change', changeTheme);
+
+function changeTheme(event) {
+  console.log(' меняем тему !!!');
+  console.log(themeSwitch.checked);
+  if (themeSwitch.checked) {
+    menuTheme.classList.remove('light-theme');
+    menuTheme.classList.add('dark-theme');
+  } else {
+    menuTheme.classList.remove('dark-theme');
+    menuTheme.classList.add('light-theme');
+  }
+}
+
+const Theme = {
+  LIGHT: 'light-theme',
+  DARK: 'dark-theme',
+};
